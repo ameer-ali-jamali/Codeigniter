55dc11e04e41eaede9705496c85b0f928766808b
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-06-02 17:15:39 +0500

    first commit

---
+++ app/Views/footer.php
@@ -0,0 +1,16 @@
+     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxy/1.6.1/scripts/jquery.ajaxy.min.js"
+         integrity="sha512-bztGAvCE/3+a1Oh0gUro7BHukf6v7zpzrAb3ReWAVrt+bVNNphcl2tDTKCBr5zk7iEDmQ2Bv401fX3jeVXGIcA=="
+         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+     </script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+         integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+     </script>
+     <script src="assests/app.js"></script>
+
+     </body>
+
+     </html>
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,26 @@
+<!-- include header -->
+<?php include('header.php') ?>
+
+<div class="conatiner">
+
+    <form action="" method="post">
+
+        <div class="mb-3">
+            <label for="" class="form-label">Email</label>
+            <input type="email" class="form-control" name="email" id="signInEmail" placeholder="abc@mail.com">
+        </div>
+
+        <div class="mb-3">
+            <label for="" class="form-label">Password</label>
+            <input type="email" class="form-control" name="email" id="signInEmail" placeholder="abc@mail.com">
+        </div>
+        <input type="submit" id="signInFormSubmit" name="signInFormSubmit" hidden>
+        <button class="btn btn-success" id="">Submit</button>
+
+    </form>
+</div>
+
+
+
+<!-- Include Footer -->
+<?php include('footer.php') ?>;
